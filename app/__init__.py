240c3b6c77a73de7c3052a65e3f07cf7f2da1edf
jeriae <jeria_levy2000@yahoo.com>
2020-05-06 20:45:54 -0500

    pushed original lab 3

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'enter some random passphrase'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '2525' 
-app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USERNAME'] = 'fd67973b176409'
+app.config['MAIL_PASSWORD'] = '72d78909053a43'
 
 
 mail = Mail(app)
